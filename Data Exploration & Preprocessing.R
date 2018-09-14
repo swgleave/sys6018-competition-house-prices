4819cd407ce87726d5ced89ce47b909be56b2442
swgleave <scottwgleave@gmail.com>
2018-09-14 16:11:48 -0400

    Add files via upload
    Added KNN function

--- Data Exploration & Preprocessing.R
+++ Data Exploration & Preprocessing.R
@@ -190,3 +190,40 @@ results = as.data.frame(results)
 colnames(results) <- c('Id', 'SalePrice')
 
 write.csv(results, 'submission.csv', row.names = FALSE)
+
+#test on our own model
+
+trainall = cbind(traindf, ytrain)
+testdf$ytrain = NA
+
+combinedpred = rbind(trainall, testdf)
+
+knn <- function(data, imp_col, train_cols, k=5) {
+  
+  data_complete <- data[complete.cases(data),]
+  data_incomplete <- data[!complete.cases(data),]
+  ncomplete <- length(data_complete[,1])
+  nincomplete <- length(data_incomplete[,1])
+  
+  for(j in 1:nincomplete) {
+    d <- numeric(ncomplete)
+    for(i in train_cols) 
+      d <- d + (data_complete[,i] - data_incomplete[j,i])^2
+    # indices of k-nearest neighbors
+    knn_index <- head(sort(d,index.return=T)$ix,k) 
+    nn <- sort(table(data_complete[knn_index,imp_col]), T)
+    data_incomplete[j,imp_col]<- names(nn)[1]
+  }
+  data_incomplete
+}
+
+prediction=knn(combinedpred, 384, 1:383, k=3)
+
+prediction$ytrain
+
+results = cbind(test2$Id, prediction$ytrain)
+results = as.data.frame(results)
+
+colnames(results) <- c('Id', 'SalePrice')
+
+write.csv(results, 'submission.csv', row.names = FALSE)
